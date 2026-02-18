70f85cc43020e8a8ebe65e0883483a72e4f448c3
A.M.Adahan <aksharaminoli98@gmail.com>
2026-02-18 19:17:24 +0530

    Add Node.js Promises functionality

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
+const condition = true; // Set to true for success case
+
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
+});
