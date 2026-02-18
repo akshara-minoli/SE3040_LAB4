4569fbad0e2f908ebb767edd21590d2cb2090a23
A.M.Adahan <aksharaminoli98@gmail.com>
2026-02-18 19:22:24 +0530

    Add Node.js Async/Await functionality

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const myPromise = new Promise((resolve, reject) => {
  reject('Failure!');
  }
 });
-myPromise.then((result) => {
+
+async function myFunction() {
+ try {
+ const result = await myPromise;
  console.log(result);
-}).catch((error) => {
+ } catch (error) {
  console.log(error);
-});
+ }
+}
+myFunction();
